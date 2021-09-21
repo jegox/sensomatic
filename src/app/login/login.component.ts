3e3bcc61129027c3780df4c426529a2f0988c702
Luguito <gomezfrancisco.mac@gmail.com>
2021-09-21 16:06:22 -0500

    fix: Show error when login fail
    delete: dummy data delete
    
    fix: style select in modal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,25 +10,28 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private fb: FormBuilder, private uService: UserService, private router:Router) { }
+  constructor(private fb: FormBuilder, private uService: UserService, private router: Router) { }
   loginForm: FormGroup;
   ngOnInit(): void {
     this.loginForm = this.initForm;
   }
-
+//developer@admin.com'
+//12345
   get initForm() {
     return this.fb.group({
-      'email': ['developer@admin.com', Validators.required],
-      'password': ['123456', Validators.required]
+      'email': [, Validators.required],
+      'password': [, Validators.required]
     });
   }
 
   login() {
-    this.uService.logIn(this.loginForm.getRawValue()).toPromise().then((v:any) => {
+    this.uService.logIn(this.loginForm.getRawValue()).toPromise().then((v: any) => {
       localStorage.setItem('user', JSON.stringify(v.data.user));
       localStorage.setItem('token', v.data.access_token);
     }).then(v => {
       this.router.navigate(['dashboard'])
+    }).catch(({ error }) => {
+      Swal.fire('Error', error.message, 'error')
     })
   }
 }
