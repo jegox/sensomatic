ce5aea38fa49e8484f9f5ec4bc32b2b1c3a690a9
Luguito <gomezfrancisco.mac@gmail.com>
2021-11-03 00:49:54 -0500

    feat: Tracking show dinamic dates
    feat

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -7,15 +7,18 @@ declare let google;
 })
 export class MapService implements OnInit {
     map;
+    poly;
+    polys = [];
     constructor(private ch: ChartSevice) { }
 
     ngOnInit() { }
 
     initMap(element: HTMLElement) {
         this.map = new google.maps.Map(element, {
-            center: { lat: 37.090, lng: -95.712 },
-            zoom: 4,
-            disableDefaultUI: true
+            center: { lat: 4.5709, lng: -74.2973 },
+            zoom: 6,
+            disableDefaultUI: true,
+            mapTypeControl: true,
         })
     }
 
@@ -24,9 +27,45 @@ export class MapService implements OnInit {
     }
 
     drawMarker({ latitude, longitude }) {
-        let x = new google.maps.Marker({
+        let markers = new google.maps.Marker({
             position: new google.maps.LatLng(latitude, longitude),
             map: this.map,
         })
+    }
+
+    drawPolylines(path, value) {
+        let polyline = new google.maps.Polyline({
+            path,
+            strokeColor: value < 1 ? "red" : "green",
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+        });
+        polyline.setMap(this.map);
+        this.polys.push(polyline);
+    }
+
+    get getPolylines(){
+        return this.polys;
+    }
+
+    setCenter(center){
+        this.map.setCenter(center);
+    }
+
+    setZoom(zoom:number) {
+        this.map.setZoom(zoom)
+    }
+    /**
+     * 
+     * @todo Centrar la linea general. Sin iteraciones
+     */
+    getCenterOfPoly(path) {
+        var bounds = new google.maps.LatLngBounds();
+        var points = path.getPath().getArray();
+        console.log(points)
+        for (var n = 0; n < path.length ; n++){
+            bounds.extend(path[n]);
+        }
+        this.map.fitBounds(bounds);
     }
 }
--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MapService } from '../services/map.service';
+import { FormGroup, FormControl } from '@angular/forms'
 declare let google;
 
 @Component({
@@ -11,6 +12,12 @@ declare let google;
 export class TrackingComponent implements OnInit, AfterViewInit {
   map;
   machineId: string;
+  tracking: FormGroup = new FormGroup({
+    'initial': new FormControl(),
+    'final': new FormControl()
+  });
+  min: Date = new Date(new Date().setDate(2));
+  max: Date = new Date();
   constructor(private route: ActivatedRoute, private M: MapService) { }
 
   ngOnInit(): void {
@@ -18,25 +25,49 @@ export class TrackingComponent implements OnInit, AfterViewInit {
       this.machineId = v.id;
       this.M.initMap(document.getElementById('map'));
       this.M.getDataForMap({
-        initial: new Date(new Date().setHours(19, 0, 0, 0)).toISOString(),
-        final: new Date(new Date().setHours(20, 0, 0, 0)).toISOString(),
+        initial: new Date(new Date().setHours(5, 59, 0, 0)).toISOString(),
+        final: new Date(new Date().setHours(17, 59, 0, 0)).toISOString(),
         machineId: this.machineId
       }).then(v => this.drawPoints(v))
-    })
+    });
   }
 
   ngAfterViewInit(): void {
   }
 
   drawPoints({ data }) {
-    console.log(data)
-    for (let points of data) {
-      let x = {
-        latitude: points.latitude,
-        longitude: points.longitude
-      }
-      console.log(x)
-      this.M.drawMarker(x)
+    data.map((item, index) => {
+      let path = [
+        { lat: data[index === 0 ? index : index - 1].latitude, lng: data[index === 0 ? index : index - 1].longitude },
+        { lat: data[index].latitude, lng: data[index].longitude },
+      ]
+
+      if (path.length > 0) this.M.drawPolylines(path, item.value);
+    });
+
+    this.M.setCenter({ lat: data[0].latitude, lng: data[0].longitude });
+    this.M.setZoom(9)
+    setTimeout(() => {
+      this.M.setZoom(12);
+    }, 1000)
+    // let center = this.M.getPolylines;
+    // this.M.getCenterOfPoly(center);
+  }
+
+  setFormatDate({ initial, final }) {
+    let newObj = {
+      initial: new Date(new Date(initial).setHours(5, 59, 0, 0)).toISOString(),
+      final: final ? new Date(new Date(final).setHours(17, 59, 0, 0)).toISOString() : new Date(new Date(initial).setHours(17, 59, 0, 0)).toISOString(),
     }
+    this.tracking.patchValue(newObj)
+  }
+
+  searchData() {
+    let value = this.tracking.getRawValue();
+    this.setFormatDate(value);
+    this.M.getDataForMap({
+      ...this.tracking.getRawValue(),
+      machineId: this.machineId
+    }).then(data => this.drawPoints(data))
   }
 }
